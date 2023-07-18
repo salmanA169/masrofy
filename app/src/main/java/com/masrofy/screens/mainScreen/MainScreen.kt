0781f7cf19b85087411a947ba8ad7f2b9df43dfc
salmanA169 <40442174+salmanA169@users.noreply.github.com>
2023-07-18 22:03:27 +0300

    update release

--- app/src/main/java/com/masrofy/screens/mainScreen/MainScreen.kt
+++ app/src/main/java/com/masrofy/screens/mainScreen/MainScreen.kt
@@ -2,7 +2,6 @@ package com.masrofy.screens.mainScreen
 
 import android.content.res.Configuration
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
